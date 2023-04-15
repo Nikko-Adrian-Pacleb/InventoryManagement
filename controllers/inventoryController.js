b611c8d5b1c8746d01b8b402469f09870a7921e5
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-04-14 23:27:21 -0700

    Error and Async Handler Added

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,6 +1,8 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get the overview of the inventory
 // @route GET /inventory
 // @access Private
-exports.inventory_get = (req, res, next) => {
+exports.inventory_get = asyncHandler(async (req, res, next) => {
   res.send("Get Inventory not yet implemented");
-};
+});
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/", require("./routes/indexRoute"));
 app.use("/inventory", require("./routes/inventoryRoute"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server Started on PORT:${port}`));
