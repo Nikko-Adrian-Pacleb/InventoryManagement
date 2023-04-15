f44d0416112125f4d132dec6ae77b4cdf057d018
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-04-14 23:13:59 -0700

    Initial Routers and Controllers Added

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Server Start");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/", require("./routes/indexRoute"));
+app.use("/inventory", require("./routes/inventoryRoute"));
+
+app.listen(port, () => console.log(`Server Started on PORT:${port}`));
