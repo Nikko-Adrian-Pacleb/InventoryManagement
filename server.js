09842c07f3a622ec06aa264027cbb6bf134fdd55
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-04-18 15:47:48 -0700

    Compression Added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
+const compression = require("compression");
+
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -11,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(compression());
 
 app.use("/", require("./routes/indexRoute"));
 app.use("/inventory", require("./routes/inventoryRoute"));
