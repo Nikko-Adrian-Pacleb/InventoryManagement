1251a2062bcb5159df134489e3f5913956792f3b
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-04-18 15:51:30 -0700

    Helmet added for increase security

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const compression = require("compression");
-
+const helmet = require("helmet");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -14,6 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 app.use(compression());
 
 app.use("/", require("./routes/indexRoute"));
